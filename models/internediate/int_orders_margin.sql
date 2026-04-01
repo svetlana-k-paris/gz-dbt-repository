a39601feb0d069b3b02b742b1d6e473208207714
skonoreva@gmail.com <skonoreva@gmail.com>
2026-04-01 22:13:28 +0000

    - ROUND revenue, purchase_cost and margin in int_orders_margin

--- models/internediate/int_orders_margin.sql
+++ models/internediate/int_orders_margin.sql
@@ -1,9 +1,9 @@
 SELECT
     orders_id,
     date_date,
-    SUM(revenue) AS order_revenue,
+    ROUND(SUM(revenue), 2) AS order_revenue,
     SUM(quantity) AS quantity,
-    SUM(purchase_cost) AS order_purchase_cost,
-    SUM(margin) AS order_margin
+    ROUND(SUM(purchase_cost), 2) AS order_purchase_cost,
+    ROUND(SUM(margin), 2) AS order_margin
 FROM {{ ref('int_sales_margin') }}
 GROUP BY orders_id, date_date
