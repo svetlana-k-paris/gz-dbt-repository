91d48176e89321f27e58a6d24663c83d4db60eb5
skonoreva@gmail.com <skonoreva@gmail.com>
2026-04-01 20:57:25 +0000

    - creation intermediate model int_sales_margin - name correction stg models - dbt run on corrected stg models - dbt run of the corrected int_sales_margin model

---
+++ models/internediate/int_sales_margin.sql
@@ -0,0 +1,25 @@
+WITH sales AS (
+    SELECT *
+    FROM {{ ref('stg_gz_raw_sales') }}
+),
+
+products AS (
+    SELECT *
+    FROM {{ ref('stg_gz_raw_product') }}
+)
+
+SELECT
+    sales.orders_id,
+    sales.date_date,
+    sales.revenue,
+    sales.quantity,
+
+    products.purchase_price,
+    products.products_id,
+
+    sales.quantity * products.purchase_price AS purchase_cost,
+    sales.revenue - (sales.quantity * products.purchase_price) AS margin
+
+FROM sales
+LEFT JOIN products
+    ON sales.products_id = products.products_id
